595ccc27172a41ce4df85ea5542f5c7c10c2dbcf
Drew Raines <aaraines@gmail.com>
2010-02-02 11:29:48 -0600

    Refactor validation.

--- static/js/signup.js
+++ static/js/signup.js
@@ -3,6 +3,7 @@ var img = function(where, id) {
 };
 
 SIGNUP = Object();
+SIGNUP.validatable = "#email, #firstname, #lastname";
 SIGNUP.validators = Object();
 
 SIGNUP.email_pattern =
@@ -10,12 +11,12 @@ SIGNUP.email_pattern =
 
 SIGNUP.do_get = function(form){
   var q = $(form).serialize();
-  console.log($("#firstname").val());
-  console.log($("#lastname").val());
-  console.log($("#email").val());
+  console.debug($("#firstname").val());
+  console.debug($("#lastname").val());
+  console.debug($("#email").val());
 
   $.get("../tmp/foo.html?"+q, function(payload){
-    console.log(payload);
+    console.debug(payload);
   });
 
 };
@@ -49,35 +50,65 @@ SIGNUP.alert = function(field) {
   $(id).html(img("../static/img/alert.gif"));
 };
 
+SIGNUP.clear_alerts = function() {
+  $(SIGNUP.validatable).
+    each(function(idx, el){
+           var id = "#" + el.id + "_status";
+           console.debug(id);
+           $(id).html("");
+         });
+};
+
+SIGNUP.validators.firstname = function(val) {
+  return val.trim() != "";
+};
+
+SIGNUP.validators.lastname = function(val) {
+  return val.trim() != "";
+};
+
 SIGNUP.validators.email = function(val) {
+  console.debug("testing email " + val + ": " + SIGNUP.email_pattern.test(val));
   return SIGNUP.email_pattern.test(val);
 };
 
-SIGNUP.validate = function() {
-  var vald = SIGNUP.validators[this.id];
+SIGNUP.validate = function(idx, el) {
+  var that = $(el)[0];
+  var vald = SIGNUP.validators[that.id];
+  var res = false;
   if (vald) {
-    console.log("validating \"" + this.id + "\"");
-    return SIGNUP.validators[this.id]($(this).val());
-  } else {
-    return false;
+    console.debug("validating \"" + that.id + "\"");
+    // if valid, return false!
+    res = SIGNUP.validators[that.id]($(that).val()) ? false : that.id;
   }
+  console.debug(that.id + " is invalid: " + res);
+  return res;
 };
 
-SIGNUP.is_valid = function () {
-  $(":input").map(SIGNUP.validate);
+SIGNUP.any_invalid = function() {
+  var res = $(SIGNUP.validatable).filter(SIGNUP.validate);
+  console.debug(res);
+  if (res.length == 0) {
+    return false;
+  }
+  // some were invalid
+  $(res).
+    each(function(idx, el) {
+           SIGNUP.alert(el.id);
+         });
   return true;
 };
 
 SIGNUP.submit = function(e) {
-  if (SIGNUP.is_valid()) {
+  SIGNUP.clear_alerts();
+  if (SIGNUP.any_invalid()) {
+    SIGNUP.message("We found some problems.  Please fix your submission.");
+  } else {
     SIGNUP.start_thinking();
     SIGNUP.do_get(this);
     SIGNUP.message("Done!  Would you like to <a href=\"\">add another</a>?");
     SIGNUP.stop_thinking();
     SIGNUP.hide_button();
-  } else {
-    SIGNUP.message("We found some problems.  Please fix your submission.");
-    SIGNUP.alert("email");
   }
 
   console.debug("form submitted");
