01520cb7b348eb0d1f58861076797ef946c0eba7
Drew Raines <aaraines@gmail.com>
2011-08-31 17:55:27 -0500

    Switch _sidebar & _upcoming.  Move map.

--- util/templates.py
+++ util/templates.py
@@ -13,38 +13,43 @@ def mark_down_template(path):
     except IOError:
         return Http404("The page you requested (%s) cannot be found." % path)
 
+def get_snippet(tmpl, name):
+    path = os.path.join(settings.USER_TEMPLATE_ROOT,
+                        tmpl.replace(".html", ""),
+                        name)
+    if os.path.exists(path):
+        return markdown_path(path)
+    else:
+        return markdown_path(os.path.join(settings.USER_TEMPLATE_ROOT,
+                                          "home",
+                                          name))
+
+
 def template_viewer(request, path):
     path = path.strip("/")
-    template = ""
     if path == "":
-        template = "index.html"
-        try:
-            secondary_content = markdown_path(os.path.join(settings.USER_TEMPLATE_ROOT, "welcome.txt"))
-        except IOError:
-            secondary_content = ""
+        template = "home.html"
+    elif path.endswith(".html"):
+        template = path
     else:
-        if path.endswith(".html"):
-            template = path
-        secondary_content = ""
-        ## Let's use html suffixes for now.
-        ## else:
-        ##     template = path + ".html"
+        raise Http404("tcnash: not a template %s" % path)
 
     content = mark_down_template(template)
-    try:
-        sidebar = markdown_path(os.path.join(settings.USER_TEMPLATE_ROOT, "events.txt"))
-    except IOError:
-        sidebar = ""
+    sidebar = get_snippet(template, "_sidebar.txt")
+    upcoming = get_snippet(template, "_upcoming.txt")
 
     try:
-        return render_to_response(template, {'content':content,'secondary_content':secondary_content,'sidebar':sidebar},
+        return render_to_response(template, {'content':content,
+                                             'sidebar':sidebar,
+                                             'upcoming':upcoming},
                                   context_instance=RequestContext(request))
     except TemplateDoesNotExist:
         if isinstance(content, Exception):
             raise content
         else:
-            return render_to_response("base.html",
-                                      {'content':content},
+            return render_to_response("base.html", {'content':content,
+                                                    'sidebar':sidebar,
+                                                    'upcoming':upcoming},
                                       context_instance=RequestContext(request))
 
 
